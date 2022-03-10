bd233edb44ae3909e784ceb82c12282f1132b992
Михаил Денисов <migelius.invesy@gmail.com>
2022-03-10 16:46:09 +0300

    Добавьте файлы проекта.

---
+++ Ex4/Program.cs
@@ -0,0 +1,25 @@
+﻿class program
+{ 
+    static void Main(string[] args)
+    {
+        string name;
+        string age;
+        string job;
+        string favoriteSongArtist;
+        string zodiacalSing;
+
+        Console.WriteLine("Как вас зовут?");
+        name = Console.ReadLine();
+        Console.WriteLine("Сколько вам лет?");
+        age = Console.ReadLine();
+        Console.WriteLine("Кем вы работаете?");
+        job = Console.ReadLine();
+        Console.WriteLine("Какая ваша любимая песня?");
+        favoriteSongArtist = Console.ReadLine();
+        Console.WriteLine("Какой ваш знак зодиака?");
+        zodiacalSing = Console.ReadLine();
+        Console.WriteLine("Меня зовут {0},\nМне {1} лет,\nЯ работаю {2},\nМоя любимая песня {3},\nМой знак зодиака {4}", name, age, job, favoriteSongArtist, zodiacalSing);
+
+    }
+
+}
